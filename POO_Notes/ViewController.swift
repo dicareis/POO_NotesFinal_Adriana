842daf59dc128c9fc6f53d763036846b2a71441a
eleves <eleves@mac304-02.local>
2017-08-23 10:12:04 -0400

    Initial Commit

---
+++ POO_Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POO_Notes
+//
+//  Created by eleves on 17-08-23.
+//  Copyright © 2017 GrassetSucre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
